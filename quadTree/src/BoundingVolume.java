99f6e752fb3ec3a82b34a8aad00597618a098fca
Lucas Saldanha Couto <lucas.couto99@hotmail.com>
2021-09-10 15:17:59 -0300

    Update BoundingVolume.java

--- quadTree/src/BoundingVolume.java
+++ quadTree/src/BoundingVolume.java
@@ -1,3 +1,17 @@
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Lucas Saldanha Couto
+ */
 
 public class BoundingVolume {
     
