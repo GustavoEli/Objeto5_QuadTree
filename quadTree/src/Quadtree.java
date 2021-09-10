79b6c69f05f360458f5454cec8d9b39035db59f8
MarcelinoABMO <marcelino.jesus@gmail.com>
2021-09-10 15:42:08 -0300

    Estrutura da Quadtree

--- quadTree/src/Quadtree.java
+++ quadTree/src/Quadtree.java
@@ -1,4 +1,38 @@
 
+import java.awt.Graphics;
+
+
 public class Quadtree {
     
+    private static Quadtree _get;
+    public static Quadtree get()
+    {
+        //TODO:
+        return _get;
+    }
+    
+    private Quadtree(float l, float t, float r, float b)
+    {
+        //
+    }
+    
+    public void add(BoundingVolume bv)
+    {
+        //
+    }
+    
+    public void checkCollision(BoundingVolume bv)
+    {
+        //
+    }
+    
+    public void simulateCollisions(Quadtree _this)
+    {
+        //
+    }
+    
+    public void draw(Graphics g)
+    {
+        //
+    }
 }
