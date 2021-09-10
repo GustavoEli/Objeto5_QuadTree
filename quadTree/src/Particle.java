ae40434ab3476572058801f663143d5f3d59ff13
GustavoEli <gustavoeliseu99@gmail.com>
2021-09-10 15:25:46 -0300

    fixing mistake

--- quadTree/src/Particle.java
+++ quadTree/src/Particle.java
@@ -1,9 +1,3 @@
-*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -12,7 +6,7 @@ import java.awt.Graphics;
  * @author Lucas Saldanha Couto
  */
 
-public class Particula {
+public class Particle {
     
     public static float SIZE = 4;
     
